4ca802eaf9e3bdcf619c3af495e5276e49345190
¨Gober¨ <reyesfragosoroberto@gmail.com>
2021-01-18 20:05:24 -0600

    Modify shell script to verify node_modules folder

--- hasura_events/create-lambda.sh
+++ hasura_events/create-lambda.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]; then
+    echo "Error: No arguments provided"
+    exit 1
+fi
+
 cd ../aws_lambdas
 
+if [ ! -d "./node_modules" ]; then
+    echo "Error: You need to install node modules"
+    exit 1
+fi
+
 # Rename the lambda function to index
+echo "Renaming $1 to index.js"
 mv $1.js index.js
 
 # zip only the necessary elements for the lambda
+echo "Zipping the content of the lambda"
 zip -r $1.zip ./config ./models ./template ./package.json ./package-lock.json index.js
 
 # Return the file to its origina name
-mv index.json $1.js
+echo "Returing index.js to $1"
+mv index.js $1.js
 
 # Move the zip file to its destination folder
 mv $1.zip ../hasura_events/
