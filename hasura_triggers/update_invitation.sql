38c5a309aaf0bd74e99b06f551041403e3936a31
¨Gober¨ <reyesfragosoroberto@gmail.com>
2021-01-19 21:39:24 -0600

    Replace insert role trigger to update invitation

---
+++ hasura_triggers/update_invitation.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION update_invitation()
+RETURNS trigger AS $$
+DECLARE
+    used_invitation BOOLEAN;
+BEGIN
+    -- Get the used field of invitation table
+    SELECT used FROM invitations where email = NEW.email INTO used_invitation;
+
+    -- Update the invitation table
+    IF used_invitation = false THEN
+        UPDATE invitations SET used = true WHERE email = NEW.email;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS update_invitation_trigger ON "users";
+CREATE TRIGGER update_invitation_trigger
+    AFTER INSERT ON "users"
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_invitation();
