fa180a9368b8f1cb200ca7f2e645a6673dd9df5e
¨Gober¨ <reyesfragosoroberto@gmail.com>
2021-06-24 20:07:59 -0500

    Format upload resume

--- aws_lambdas/upload-resume.js
+++ aws_lambdas/upload-resume.js
@@ -1,41 +1,31 @@
-const fs = require('fs')
-const {
-  GoogleFactory,
-  DRIVE_API,
-  PDF_TYPE,
-  FOLDER_TYPE
-} = require('./models')
+const fs = require("fs");
+const { GoogleFactory, DRIVE_API, PDF_TYPE, FOLDER_TYPE } = require("./models");
 
 exports.handler = async (event) => {
-  const encoding = 'base64'
-  const filename = 'resume.pdf'
+  const encoding = "base64";
+  const filename = "resume.pdf";
 
-  const parsedBody = JSON.parse(event.body)
-  const userId = parsedBody.session_variables['x-hasura-user-id']
-  const resume = parsedBody.input.resume
+  const parsedBody = JSON.parse(event.body);
+  const userId = parsedBody.session_variables["x-hasura-user-id"];
+  const resume = parsedBody.input.resume;
 
-  const fileBuffer = Buffer.from(resume, encoding)
-  const path = `/tmp/${filename}`
+  const fileBuffer = Buffer.from(resume, encoding);
+  const path = `/tmp/${filename}`;
 
   try {
-    fs.writeFileSync(path, fileBuffer, encoding)
+    fs.writeFileSync(path, fileBuffer, encoding);
   } catch (_error) {
-    return { statusCode: 422 }
+    return { statusCode: 422 };
   }
 
-  const driveAPI = new GoogleFactory(DRIVE_API)
-  const folderId = await driveAPI.createResource(userId, FOLDER_TYPE)
+  const driveAPI = new GoogleFactory(DRIVE_API);
+  const folderId = await driveAPI.createResource(userId, FOLDER_TYPE);
 
-  const readStream = fs.createReadStream(path)
-  await driveAPI.createResource(
-    filename,
-    PDF_TYPE,
-    folderId,
-    readStream
-  )
+  const readStream = fs.createReadStream(path);
+  await driveAPI.createResource(filename, PDF_TYPE, folderId, readStream);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ id: folderId })
-  }
-}
+    body: JSON.stringify({ id: folderId }),
+  };
+};
