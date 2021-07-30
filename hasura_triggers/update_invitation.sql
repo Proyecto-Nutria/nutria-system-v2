21049ee2aeffbf74823969710d1b0be1438ad903
¨Gober¨ <reyesfragosoroberto@gmail.com>
2021-07-30 14:13:21 -0500

    Change trigger table to update invitation

--- hasura_triggers/update_invitation.sql
+++ hasura_triggers/update_invitation.sql
@@ -2,21 +2,23 @@ CREATE OR REPLACE FUNCTION update_invitation()
 RETURNS trigger AS $$
 DECLARE
     used_invitation BOOLEAN;
+    user_email text;
 BEGIN
     -- Get the used field of invitation table
-    SELECT used FROM invitations WHERE email = NEW.email INTO used_invitation;
+    SELECT email FROM users WHERE auth0_id = NEW.user_id INTO user_email;
+    SELECT used FROM invitations WHERE email = user_email INTO used_invitation;
 
     -- Update the invitation table
     IF used_invitation = false THEN
-        UPDATE invitations SET used = true WHERE email = NEW.email;
+        UPDATE invitations SET used = true WHERE email = user_email;
     END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS update_invitation_trigger ON "users";
+DROP TRIGGER IF EXISTS update_invitation_trigger ON "interviewees";
 CREATE TRIGGER update_invitation_trigger
-    AFTER INSERT ON "users"
+    AFTER INSERT ON "interviewees"
     FOR EACH ROW
     EXECUTE PROCEDURE update_invitation();
