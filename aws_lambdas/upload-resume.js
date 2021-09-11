c0ccbe2476c7a1a151f7e6bb9cce6ccffed02404
¨Gober¨ <reyesfragosoroberto@gmail.com>
2021-09-10 22:01:52 -0500

    Migrate logic to upload interviewee to backend

--- aws_lambdas/upload-resume.js
+++ aws_lambdas/upload-resume.js
@@ -1,4 +1,6 @@
+const request = require("request-promise");
 const fs = require("fs");
+
 const { GoogleFactory, DRIVE_API, PDF_TYPE, FOLDER_TYPE } = require("./models");
 
 exports.handler = async (event) => {
@@ -9,6 +11,13 @@ exports.handler = async (event) => {
   const intervieweeId = parsedBody.session_variables["x-hasura-user-id"];
   const resume = parsedBody.input.resume;
   const newUser = parsedBody.input.firstTime;
+  const school = parsedBody.input.school;
+
+  const user_role = parsedBody.session_variables["x-hasura-role"];
+  const user_id = parsedBody.session_variables["x-hasura-user-id"];
+
+  const adminSecret = process.env.ACCESS_KEY;
+  const url = process.env.HASURA_URL;
 
   const fileBuffer = Buffer.from(resume, encoding);
   const path = `/tmp/${filename}`;
@@ -30,8 +39,63 @@ exports.handler = async (event) => {
   const readStream = fs.createReadStream(path);
   await driveAPI.createResource(filename, PDF_TYPE, folderId, readStream);
 
+  const newUserMutation = `mutation createInterviewee($information: interviewees_insert_input!) {
+    insert_interviewees_one(object: $information) {
+      school
+    }
+  }`;
+  const updateUserMutation = `mutation updateInterviewee($school: interviewee_school_enum!) {
+    update_interviewees(where: {}, _set: { school: $school }) {
+      returning {
+        school
+      }
+    }
+  }`;
+
+  const userHeader = {
+    "content-type": "application/json",
+    "x-hasura-admin-secret": adminSecret,
+    "x-hasura-role": user_role,
+    "x-hasura-user-id": user_id,
+  };
+
+  const newUserOptions = {
+    headers: userHeader,
+    url: url,
+    body: JSON.stringify({
+      query: newUserMutation,
+      variables: {
+        information: {
+          folder: folderId,
+          school: school,
+        },
+      },
+    }),
+  };
+  const updateUserOptions = {
+    headers: userHeader,
+    url: url,
+    body: JSON.stringify({
+      query: updateUserMutation,
+      variables: {
+        school: school,
+      },
+    }),
+  };
+
+  const options = newUser ? newUserOptions : updateUserOptions;
+
+  await request.post(options, (error, _response, _body) => {
+    if (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ result: 500 }),
+      };
+    }
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ id: folderId }),
+    body: JSON.stringify({ result: 200 }),
   };
 };
